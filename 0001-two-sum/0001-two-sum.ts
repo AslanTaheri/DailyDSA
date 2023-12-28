1203a220e30d7207f0af9895af9f563b21a49e25
Aslan <123320141+AslanTaheri@users.noreply.github.com>
2023-12-27 19:35:23 -0500

    Time: 54 ms (91.64%), Space: 45.7 MB (18.04%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.ts
@@ -0,0 +1,14 @@
+function twoSum(nums: number[], target: number): number[] {
+    
+    const set = new Set<number>();
+    
+    for (let i = 0; i<nums.length; i++){
+        let complement = target - nums[i];
+        if (set.has(complement)){
+            return [i, nums.indexOf(complement)];
+        }else{
+            set.add(nums[i])
+        }
+    }
+    
+};
