849b8b7ecb944ac1be9e4ac7f1bc6e94378f2591
Aslan <123320141+AslanTaheri@users.noreply.github.com>
2023-12-29 13:01:08 -0500

    Time: 81 ms (87.9%), Space: 53.6 MB (77.37%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -1,26 +1,48 @@
+// Time: O(n)
+// Space: O(1)
 function productExceptSelf(nums: number[]): number[] {
+    const product: number[] = [];
+    product[0] = 1;
 
-    const prefix = new Map<number, number>()
-    const suffix = new Map<number, number>()
-
-    // Multiplying the numbers before the idx
-    for (let i = 0; i < nums.length; i++) {
-        const preFactor = (prefix.get(i - 1) ?? 1) * (nums[i - 1] ?? 1);
-        prefix.set(i, preFactor);
+    // first traverse to build the pre-fix
+    for (let i = 1; i < nums.length; i++) {
+        product[i] = product[i - 1] * nums[i-1];
     }
 
-    // Multiplying the numbers after the idx
+    // second traverse to multiply the post-fix
+    let postFix = 1;
     for (let i = nums.length - 1; i >= 0; i--) {
-        const postFactor = (suffix.get(i + 1) ?? 1) * (nums[i + 1] ?? 1);
-        suffix.set(i, postFactor);
+        product[i] *= postFix;
+        postFix *= nums[i];
     }
+    return product;
+}
 
-    // Bulding the product
-    const product: number[] = []
-    for (let i = 0; i < nums.length; i++) {
-        product.push(
-            prefix.get(i) * suffix.get(i)
-        )
-    }
-    return product
-};
+// // Time: O(n)
+// // Space: O(n)
+// function productExceptSelf(nums: number[]): number[] {
+
+//     const prefix = new Map<number, number>()
+//     const suffix = new Map<number, number>()
+
+//     // Multiplying the numbers before the idx
+//     for (let i = 0; i < nums.length; i++) {
+//         const preFactor = (prefix.get(i - 1) ?? 1) * (nums[i - 1] ?? 1);
+//         prefix.set(i, preFactor);
+//     }
+
+//     // Multiplying the numbers after the idx
+//     for (let i = nums.length - 1; i >= 0; i--) {
+//         const postFactor = (suffix.get(i + 1) ?? 1) * (nums[i + 1] ?? 1);
+//         suffix.set(i, postFactor);
+//     }
+
+//     // Bulding the product
+//     const product: number[] = []
+//     for (let i = 0; i < nums.length; i++) {
+//         product.push(
+//             prefix.get(i) * suffix.get(i)
+//         )
+//     }
+//     return product
+// };
