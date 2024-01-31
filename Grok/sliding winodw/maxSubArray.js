b9cc278f85b3756001513502b602ee8f58882c57
Aslan <aslan.thr@gmail.com>
2024-01-31 01:09:55 -0500

    solved maxSubArray

--- Grok/sliding winodw/maxSubArray.js
+++ Grok/sliding winodw/maxSubArray.js
@@ -1,16 +1,25 @@
 function maxSubArray(arr, k) {
-  let windowStart = 0;
+  let [winStart, index, sum, max] = Array(4).fill(0);
 
-  let sum = 0;
-  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-    windowStart += 1;
-    sum += arr[windowEnd];
+  for (let winEnd = 0; winEnd < arr.length; winEnd++) {
+    sum += arr[winEnd];
 
     //check if window size has reached k
-    if (windowStart >= k) {
-      //slide the window
+    if (winEnd >= k - 1) {
+      if (sum > max) {
+        max = sum;
+        index = winStart;
+      }
 
-      sum -= arr[windowStart];
+      //slide the window
+      sum -= arr[winStart];
+      winStart += 1;
     }
   }
+  console.log(max);
+  console.log(arr.slice(index, index + k));
 }
+
+//test case
+maxSubArray([2, 1, 5, 1, 3, 2], 3);
+maxSubArray([2, 3, 4, 1, 5], 2);
